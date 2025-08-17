dc06772990e15784e8f5b67fe10beb45291696ec
cyphenstine <cyphenstine01@gmail.com>
2025-08-17 16:17:42 +0530

    first commit

--- pcaProject/libraryMgmt.java
+++ pcaProject/libraryMgmt.java
@@ -1,15 +1,48 @@
 class LibraryManagement{
+    public static void main(String[] args){
 
+    }
 }
 
-class Books{
-    private boolean bookAvailability;
+class Book{
+
+    private String title;
+    private String author;
+    private boolean availability;
+    
+    // this is called a constructor:
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.availability = true;
+    }
+    
 }
 
 class Members{
 
+    private String name;
+    private String userID;
+
+    Members(String name, String userID){
+        this.name = name;
+        this.userID = userID;
+    }
 }
 
 class Library{
 
+    private String userID;
+    private String title;
+
+    Library(String userId, String title){
+        this.userID = userId;
+        this.title = title;
+    }
+
+    Book bk = new Book(title, null);
+    Members mb = new Members(null, userID);
+
+    
 }
