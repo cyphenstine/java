fa4de0cf163aa531197b301b1ed1198577b6910e
cyphenstine <cyphenstine01@gmail.com>
2025-09-01 03:05:06 +0530

    updated JavaString.java

--- array/arrays.java
+++ array/arrays.java
@@ -6,32 +6,32 @@ class Arrays {
 
 		char arr2[] = new char[10];
 		System.out.println(arr2[0]);*/
-		
+
         int[] marks = { 90, 80, 88, 96, 100 };
         // System.out.println(marks[9]);
-        
+
         // for each loop:
-    
+
         // for(int number: marks){
         //     System.out.println(number);
         // }
-        
+
         // int sum = 0;
-        
+
         // for(int number: marks){
         //     sum += number;
         // }
-        
+
         // System.out.println("the total is: " + sum);
-        
+
         int min = marks[0];
-        
-        for(int number: marks){
-            if(number < min){
-                 min = number;
+
+        for (int number : marks) {
+            if (number < min) {
+                min = number;
             }
         }
-        
+
         System.out.println(min);
     }
 }
