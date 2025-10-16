544048cabb9d749429e61d5f9fee10ab1e028995
cyphenstine <cyphenstine01@gmail.com>
2025-10-16 22:34:08 +0530

    made few changes in some files.

--- string/StringMethods.java
+++ string/StringMethods.java
@@ -1,5 +1,12 @@
 class StringMethods{
     public static void main(String[] args){
+        String myFirstName = "Aritra";
+        System.out.println(myFirstName.toUpperCase());
+        System.out.println(myFirstName.toLowerCase());
+        String myLastName = "  Kumar  ";
+        System.out.println(myLastName.trim()); // trims spaces
+        String myOtherName = "cyphenstine";
+        String startsWithO = "Oscar";
 
     }
 }
