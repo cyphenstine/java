228ae5ca1c70fe4047ab525afd2e06b9ce4a3fbc
cyphenstine <cyphenstine01@gmail.com>
2025-10-20 13:06:46 +0530

    made few changes in some files.

---
+++ pca2/q2/negToPos.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Enter you text: ");
+    Scanner input = new Scanner(System.in);
+    String userInput = input.nextLine();
+    //System.out.println(userInput);
+    for(char c: userInput.
+    
+  }
+
+}
