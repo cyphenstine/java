696b37c65de531a7ca6723c7bf7e039d3f21a0db
cyphenstine <cyphenstine01@gmail.com>
2025-12-17 12:20:51 +0530

    complited q7

--- pca2_questions/q7/Main.java
+++ pca2_questions/q7/Main.java
@@ -1,9 +1,37 @@
 package q7;
 
+import java.util.HashSet;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        System.out.println(returUniqueSubStringCount("pwwkew"));
+    }
+
+    public static int returUniqueSubStringCount(String str)
+    {
+        int left = 0;
+        int right = 0;
+        int maxLength = 0;
+        HashSet<Character> set = new HashSet<>();
+        while (right < str.length()) 
+        {
+            char currentChar = str.charAt(right);
+
+            if(!set.contains(currentChar))
+            {
+                set.add(currentChar);
+                maxLength = Math.max(maxLength, right-left+1);
+                right++;
+            }
+
+            else
+            {
+                set.remove(str.charAt(left));
+                left++;
+            }
+        }
+        return maxLength;
     }
 }
