155e8d96235c1f7391e325422787857a9340bb23
cyphenstine <cyphenstine01@gmail.com>
2025-12-23 15:39:07 +0530

    added some things

--- StringMethods.java
+++ StringMethods.java
@@ -8,5 +8,7 @@ class StringMethods{
         String myOtherName = "cyphenstine";
         String startsWithO = "Oscar";
 
+        int value = myOtherName.compareTo(startsWithO);//if positive then first string has a bigger value, if negetive then has a smaller value, if 0 then same. a has less value than b.
+        System.out.print(value);
     }
 }
